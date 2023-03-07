a560b6d710ee1d8ecafc23c7f56e4653d5fb140c
dominicmxrtinez <96898365+dominicmxrtinez@users.noreply.github.com>
2023-03-06 18:46:25 -0600

    Returns string repeated based on input

---
+++ 8 Kyu/string-repeat.js
@@ -0,0 +1,20 @@
+/*
+DESCRIPTION:
+
+Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+Examples (input -> output)
+
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+*/
+
+const repeatStr = (n, s) => {
+  
+  let startStr = '';
+  
+  for (let i = 0; i < n; i++) {
+    startStr = startStr + s;
+  }
+  return startStr;
+}
